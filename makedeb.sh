0a892bb349118164e9c6f12958ef26431fee6b7b
CylonicRaider <CylonicRaider@users.noreply.github.com>
2017-03-01 22:55:24 +0100

    Add variables

--- makedeb.sh
+++ makedeb.sh
@@ -6,9 +6,15 @@
 
 exec make -f- -- "$@" _SCRIPT="$0" <<'EOF'
 
+BUILD = build
+DATA = data
+
 .PHONY: help
 
 help:
 	@echo "USAGE: $(_SCRIPT) help|clean|<something>.deb [VAR=value ...]"
+	@echo "Variables:"
+	@echo "  BUILD: The directory where to place temporary build files"
+	@echo "  DATA: The directory to take package data from"
 
 EOF
